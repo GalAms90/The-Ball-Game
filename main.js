17833c760569c8388c0309304303a5703560886f
GalAms90 <Gams2597@gmail.com>
2025-03-05 15:20:33 +0200

    add change color to ball functionality

--- main.js
+++ main.js
@@ -4,9 +4,13 @@
 function onBallClick(elBall) {
 
   var currentSize = elBall.offsetWidth;
-  var newSize = currentSize + getRandomInt(20, 61);
+  var randomSize = getRandomInt(20, 61)
+  var newSize = currentSize + randomSize
 
   newSize > 400 ? newSize = 100 : newSize
+
+  var randomColor = getRandomColor()
+  elBall.style.backgroundColor = randomColor
 
   elBall.style.width = newSize + 'px';
   elBall.style.height = newSize + 'px';
