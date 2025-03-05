cd44f5207ed54cdbe01eb7e04eb988cefae2878e
GalAms90 <Gams2597@gmail.com>
2025-03-05 15:07:32 +0200

    limit ball size

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ function onBallClick(elBall) {
   var currentSize = elBall.offsetWidth;
   var newSize = currentSize + 50;
 
+  newSize > 400 ? newSize = 100 : newSize
+
   elBall.style.width = newSize + 'px';
   elBall.style.height = newSize + 'px';
   elBall.innerText = newSize;
